7eeb2db8e1840a0aad79808dd0be373b48b8e06e
phconzatti <143150868+phconzatti@users.noreply.github.com>
2024-06-19 20:38:33 -0300

    1

--- src/App.java
+++ src/App.java
@@ -1,2 +1,14 @@
-public class App {
-}
+import javax.swing.*;
+public class App extends JFrame {
+        private Tela tela;
+        public App() {
+            super();
+            tela = new Tela();
+            JPanel painel = tela.getPainel();
+            add(painel);
+            setSize(800,600);
+            setDefaultCloseOperation(EXIT_ON_CLOSE);
+            setVisible(true);
+        }
+    }
+
--- src/Tela.java
+++ src/Tela.java
@@ -1,6 +1,60 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Tela {
+    private JPanel painel;
     private JTextField codigo;
     private JTextField nome;
+    private JRadioButton individual;
+    private JRadioButton empresa;
+    private JButton confirma;
+    private JButton sair;
+    private JButton limpar;
+    private JTextField anoCpf;
+    private JButton imprime;
+    private JTextArea exibeDados;
+    private ButtonGroup b;
+
+    public Tela (){
+
+        confirma.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String texto = anoCpf.getText();
+                exibeDados.append(texto);
+                if (individual.isSelected()){
+
+                    exibeDados.append("individual");
+                }
+                if (empresa.isSelected()){
+                    exibeDados.append("empresarial");
+                }
+            }
+        });
+        imprime.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                exibeDados.append("fkjfjfjfj\n");
+            }
+        });
+        limpar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                codigo.setText("");
+                nome.setText("");
+                exibeDados.setText("");
+            }
+        });
+        sair.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+
+    }
+    public JPanel getPainel(){
+        return painel;
+    }
 }
